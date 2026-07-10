f0eb25791557419ea72461713afa87981514b3b0
Omar-gamal188 <omargamal334i@gmail.com>
2026-07-10 17:36:08 +0300

    Describe your changes

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,30 @@
-const app = require('./app');
-const connectDB = require('./config/db');
-const env = require('./config/env');
+const app = require("./app");
+const connectDB = require("./config/db");
+const env = require("./config/env");
 
-async function start() {
-  await connectDB();
-  app.listen(env.port, () => {
-    console.log(`[server] listening on port ${env.port} (${env.nodeEnv})`);
-  });
+let isConnected = false;
+
+async function init() {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+    console.log("MongoDB Connected");
+  }
 }
 
-start();
+// تشغيل محلي
+if (process.env.VERCEL !== "1") {
+  (async () => {
+    await init();
+
+    app.listen(env.port, () => {
+      console.log(`[server] listening on port ${env.port} (${env.nodeEnv})`);
+    });
+  })();
+}
+
+// تشغيل على Vercel
+module.exports = async (req, res) => {
+  await init();
+  return app(req, res);
+};
